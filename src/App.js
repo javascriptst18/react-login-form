a35e8f1da2c8c48f7cfba058d11272d5f60eb955
Jesper Orb <jesper.orb@zocom.se>
2018-08-13 15:24:35 +0200

    Change handlers and state

--- src/App.js
+++ src/App.js
@@ -7,24 +7,43 @@ import './App.css';
  */
 
 class App extends Component {
+
+  state = {
+    loggedIn: false,
+    email: '',
+    password: ''
+  }
+
+  // This will happen last
+  onSubmit = () => {
+
+  }
+
+  // Every input needs an event handler
+  handleEmail = (event) => {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePassword = (event) => {
+    this.setState({ password: event.target.value });
+  }
+  
   render() {
     return (
       <div className="App">
         <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input onChange={this.handleEmail} value={this.state.email}
+              type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input onChange={this.handlePassword} value={this.state.password}
+              type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
           </form>
         </div>
       );
