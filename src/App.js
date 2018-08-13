9665d96a94ba851fddf6696654c7cd1e53e21009
Jesper Orb <jesper.orb@zocom.se>
2018-08-13 16:00:44 +0200

    Generic change handler and named inputs

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
   state = {
     loggedIn: false,
     email: '',
-    password: ''
+    password: '',
+    passwordError: '',
+    emailError: ''
   }
 
   // We need to submit the form
   onSubmit = (event) => {
+    // Always prevent the form from submitting
     event.preventDefault();
+    /**
+     * If email and password is not empty, change the logged in state to true
+     */
     if(this.state.email && this.state.password){
       this.setState({ loggedIn: true });
     }
@@ -30,6 +36,11 @@ class App extends Component {
   handlePassword = (event) => {
     this.setState({ password: event.target.value });
   }
+
+  // This replaces both handleEmail and handlePassword
+  handleChange = (event) => {
+    this.setState({ [event.target.name] : event.target.value });
+  }
   
   render() {
     return (
@@ -38,13 +49,13 @@ class App extends Component {
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
             { /* Controlled Component */}
-            <input onChange={this.handleEmail} value={this.state.email}
+            <input name="email" onChange={this.handleEmail} value={this.state.email}
               type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
               <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
-            <input onChange={this.handlePassword} value={this.state.password}
+            <input name="password" onChange={this.handlePassword} value={this.state.password}
               type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
