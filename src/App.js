00e0ebed805a7291132145bc71086dca7b582866
Jesper Orb <jesper.orb@zocom.se>
2018-08-14 08:28:06 +0200

    Onchange handle change

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ class App extends Component {
   state = {
     loggedIn: false,
     email: '',
-    password: '',
-    passwordError: '',
-    emailError: ''
+    password: ''
   }
 
   // We need to submit the form
@@ -28,15 +26,6 @@ class App extends Component {
     }
   }
 
-  // Every input needs an event handler
-  handleEmail = (event) => {
-    this.setState({ email: event.target.value });
-  }
-
-  handlePassword = (event) => {
-    this.setState({ password: event.target.value });
-  }
-
   // This replaces both handleEmail and handlePassword
   handleChange = (event) => {
     this.setState({ [event.target.name] : event.target.value });
@@ -48,14 +37,13 @@ class App extends Component {
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
-            { /* Controlled Component */}
-            <input name="email" onChange={this.handleEmail} value={this.state.email}
+            <input name="email" onChange={this.handleChange} value={this.state.email}
               type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
               <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
-            <input name="password" onChange={this.handlePassword} value={this.state.password}
+            <input name="password" onChange={this.handleChange} value={this.state.password}
               type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
